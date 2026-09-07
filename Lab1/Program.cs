9f1cda6cec65184a0d1c0903f1b792f9a9d4f321
Ethan Ashmore <ethankashmore@gmail.com>
2026-09-07 18:03:16 -0400

    Added Transaction class and tested it

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Lab1;
+
+Transaction test = new Transaction(3, "Deposit", 7602);
+
+Console.WriteLine(test.Id);
+Console.WriteLine(test.Kind);
+Console.WriteLine(test.Amount);
+Console.WriteLine(test.IsDeposit());
+Console.WriteLine(test.Describe());
---
+++ Lab1/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Lab1;
+
+//This class shows money moving from one place to another
+public class Transaction
+{
+    //Properties
+    public int Id { get; private set; }
+    public string Kind { get; private set; }
+    public double Amount { get; private set; }
+
+    //Constructor
+    public Transaction(int id, string kind, double amount)
+    {
+        Id = id;
+        Kind = kind;
+        Amount = amount;
+    }
+
+    //Methods
+    public bool IsDeposit()
+    {
+        return Kind == "Deposit";
+    }
+
+    public string Describe()
+    {
+        return string.Format("{0,4}  {1,-12} ${2,10:N2}", Id, Kind, Amount);
+    }
+}
